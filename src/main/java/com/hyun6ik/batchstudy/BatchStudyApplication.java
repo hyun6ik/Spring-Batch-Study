362db77e23a1b5534345817d28dee43c96f9ac8d
hyun6ik <fkanwm@naver.com>
2021-12-30 17:57:23 +0900

    Initial commit

---
+++ src/main/java/com/hyun6ik/batchstudy/BatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hyun6ik.batchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchStudyApplication.class, args);
+    }
+
+}
